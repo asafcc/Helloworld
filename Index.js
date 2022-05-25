ecd72acbc97c19bcdfad98ce76dc9179e28148f0
asafcc <64134473+asafcc@users.noreply.github.com>
2022-05-25 12:09:25 +0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,4 +1,8 @@
 setTimeout(()=>{
-const input = document.getElementsByClassName("qa-email-textbox")[0]
-console.log(input && input.value);
+const input = document.getElementsByClassName("qa-email-textbox")[0];
+console.log("from script");
+if(input){
+  console.log(input.value);
+}
+
 },2000);
